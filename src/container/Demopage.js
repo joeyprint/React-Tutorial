91977233d47dbc9d299c6243656019619c689f29
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2020-11-17 08:22:17 +0700

    :lipstick: add content

--- src/container/Demopage.js
+++ src/container/Demopage.js
@@ -32,6 +32,11 @@ const Demo = props => (
             tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
             magna mollis euismod. Donec sed odio dui.</p>
         </Content>
+        <Content title="Heading" size={4} >
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo,
+            tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
+            magna mollis euismod. Donec sed odio dui.</p>
+        </Content>
       </Row>
     </Grid>
 
